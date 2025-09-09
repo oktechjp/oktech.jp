28e6b84f374b7585fa1cab70a91ea56a93dc6df5
クリス.コム <git@xn--pckua0m.xn--tckwe>
2025-09-09 01:11:49 +0000

    Topbar focus

--- src/components/Common/TopBar.tsx
+++ src/components/Common/TopBar.tsx
@@ -6,6 +6,7 @@ import Brand from "@/components/Common/Brand";
 import Container from "@/components/Common/Container";
 import Link from "@/components/Common/Link";
 import { MENU } from "@/constants";
+import { useFocus } from "@/utils/hooks/useFocus";
 
 function GlassCell({
   children,
@@ -28,7 +29,7 @@ function GlassCell({
 export default function TopBar() {
   const items = MENU.filter((item) => item.header === true);
   const [showBackground, setShowBackground] = useState(false);
-  const [logoActive, setLogoActive] = useState(false);
+  const logoActive = useFocus();
 
   useEffect(() => {
     let last = window.scrollY > 0;
@@ -53,24 +54,6 @@ export default function TopBar() {
     return () => window.removeEventListener("scroll", onScroll);
   }, []);
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setLogoActive(true);
-    }, 500);
-
-    const handleFocus = () => setLogoActive(true);
-    const handleBlur = () => setLogoActive(false);
-
-    window.addEventListener("focus", handleFocus);
-    window.addEventListener("blur", handleBlur);
-
-    return () => {
-      clearTimeout(timer);
-      window.removeEventListener("focus", handleFocus);
-      window.removeEventListener("blur", handleBlur);
-    };
-  }, []);
-
   return (
     <div data-testid="top-bar" className="fixed top-0 z-50 w-full">
       <Container>
@@ -78,7 +61,7 @@ export default function TopBar() {
           <GlassCell showBackground={showBackground} testId="navbar-logo">
             <Link
               href="/"
-              className="group btn btn-glass sm:btn-lg md:btn-xl rounded-field"
+              className="group btn btn-glass sm:btn-lg md:btn-xl rounded-field opacity-90 transition-opacity hover:opacity-100 active:opacity-90"
               data-astro-prefetch
             >
               <div className="-mr-1 -ml-2">
@@ -95,6 +78,7 @@ export default function TopBar() {
                   data-astro-prefetch
                   className={clsx(
                     "btn btn-glass btn-md sm:btn-lg md:btn-xl rounded-field font-bold",
+                    "opacity-90 transition-opacity hover:opacity-100 active:opacity-90",
                     // "-mr-1 -ml-1",
                     i > 0 && "-ml-2",
                     // i === items.length - 1 && "-mr-0",
