5034c3ee44421df1ff69ad1fe9cb86bcfa5b8b94
Martin Heidegger <martin.heidegger@gmail.com>
2025-09-11 10:35:35 +0900

    fix: hiding instructional photos

--- scripts/import-data/lib/photos.ts
+++ scripts/import-data/lib/photos.ts
@@ -15,6 +15,7 @@ export type ExternalPhoto = {
   res?: [number, number][];
   corners?: string[];
   removed?: boolean;
+  instructional?: boolean;
 };
 
 export type ExternalPhotoJSON = {
@@ -57,7 +58,7 @@ export class PhotoService {
         if (!photosByEvent[eventId]) {
           photosByEvent[eventId] = [];
         }
-        photosByEvent[eventId].push(...batch.photos);
+        photosByEvent[eventId].push(...batch.photos.filter(photo => ! photo.instructional));
         assignedCount++;
 
         if (config.photoEventPatches[batch.timestamp]) {
