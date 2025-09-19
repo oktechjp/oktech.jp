47fef15ad7c3be46a1089fcff9840fc7dd12a56d
クリス.コム <git@xn--pckua0m.xn--tckwe>
2025-09-19 02:45:07 +0000

    Bouncy cards

--- src/components/Common/EventCard.tsx
+++ src/components/Common/EventCard.tsx
@@ -1,5 +1,6 @@
 import { memo } from "react";
 
+import { animated, useSpring } from "@react-spring/web";
 import clsx from "clsx";
 
 import CityBadge from "@/components/Common/CityBadge";
@@ -28,35 +29,50 @@ const EventCard = memo(function EventCard({
   const border = index === undefined;
   const eventUrl = `/events/${event.id}`;
 
+  const [springs, api] = useSpring(() => ({
+    scale: 1,
+    config: { mass: 0.5, tension: 300, friction: 10 },
+  }));
+
   return (
-    <Link
-      href={eventUrl}
-      data-astro-prefetch
-      className={clsx(
-        "ok-card",
-        // border && "rounded-box overflow-hidden",
-        // !border && "border-t-0 border-r-0 border-l-0",
-        variant === "compact" && "flex items-center",
-        variant !== "compact" && "rounded-box",
-        variant === "polaroid" && "flex flex-col",
-        variant === "big" && "flex flex-col sm:grid sm:grid-cols-2",
-      )}
-      data-testid={`event-card-${event.id}`}
+    <animated.div
+      style={{
+        transform: springs.scale.to((s) => `scale3d(${s}, ${s}, 1)`),
+        transformStyle: "preserve-3d",
+        willChange: "transform",
+      }}
+      onMouseEnter={() => api.start({ scale: 1.05 })}
+      onMouseLeave={() => api.start({ scale: 1 })}
     >
-      <EventCardImage
-        event={event}
-        variant={variant}
-        first={first}
-        last={last}
-        cityComponent={
-          <CityBadge
-            city={event.venue?.city}
-            className="rounded-tr-none rounded-br-none rounded-bl-none pl-4"
-          />
-        }
-      />
-      <EventCardDescription event={event} variant={variant} />
-    </Link>
+      <Link
+        href={eventUrl}
+        data-astro-prefetch
+        className={clsx(
+          "ok-card",
+          // border && "rounded-box overflow-hidden",
+          // !border && "border-t-0 border-r-0 border-l-0",
+          variant === "compact" && "flex items-center",
+          variant !== "compact" && "rounded-box",
+          variant === "polaroid" && "flex flex-col",
+          variant === "big" && "flex flex-col sm:grid sm:grid-cols-2",
+        )}
+        data-testid={`event-card-${event.id}`}
+      >
+        <EventCardImage
+          event={event}
+          variant={variant}
+          first={first}
+          last={last}
+          cityComponent={
+            <CityBadge
+              city={event.venue?.city}
+              className="rounded-tr-none rounded-br-none rounded-bl-none pl-4"
+            />
+          }
+        />
+        <EventCardDescription event={event} variant={variant} />
+      </Link>
+    </animated.div>
   );
 });
 
