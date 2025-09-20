3b4312efd9695dc5e934cee45b49f4526fa25d63
クリス.コム <git@xn--pckua0m.xn--tckwe>
2025-09-20 23:17:48 +0000

    Enable  devtools

--- src/components/Common/DevTools.tsx
+++ src/components/Common/DevTools.tsx
@@ -3,7 +3,6 @@ import { useEffect, useState } from "react";
 import { useBreakpoint } from "@/utils/hooks/useBreakpoint";
 
 export default function DevTools() {
-  return null;
   const [width, setWidth] = useState(0);
   const currentBreakpoint = useBreakpoint();
 
@@ -15,10 +14,10 @@ export default function DevTools() {
   }, []);
 
   return (
-    <div
-      className={`fixed bottom-4 left-1/2 z-50 -translate-x-1/2 rounded-full bg-red-500 px-3 py-1.5 font-mono text-sm text-white sm:bg-orange-500 md:bg-yellow-500 lg:bg-green-500 xl:bg-blue-500 2xl:bg-purple-500`}
-    >
-      <span className="font-bold">{currentBreakpoint}</span>: {width}px
+    <div className={`fixed bottom-2 left-2 z-50`}>
+      <div className="rounded-full bg-red-500 px-2 py-1 font-mono text-sm text-white sm:bg-orange-500 md:bg-yellow-500 lg:bg-green-500 xl:bg-blue-500 2xl:bg-purple-500">
+        <span className="font-bold">{currentBreakpoint}</span>: {width}px
+      </div>
     </div>
   );
 }
